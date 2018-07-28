9516a8b4bc6342578336ed5a9d8e1a822a506d7b
npab19 <npab19@gmail.com>
2018-07-28 12:51:54 -0400

    Created a script to enable MFA for all global admins

---
+++ Automate-2/Automate/Enable MFA for all global admins.ps1
@@ -0,0 +1,22 @@
+﻿# Remove all existing Powershell sessions 
+Get-PSSession | Remove-PSSession 
+
+# Authenticate to Office 365
+$Credentials = Get-AutomationPSCredential -Name "Office 365"
+Connect-MsolService -Credential $Credentials
+
+# Set Authentication Requirement parameter
+$MFA = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+$MFA.RelyingParty = "*"
+
+# Choose the MFA State
+$MFA.State = "Enforced"
+
+# Get todays date and set it as a parameter for not issue before date 
+$MFA.RememberDevicesNotIssuedBefore = (Get-Date)
+
+#Set MFA for all Global Admins
+$Global_Admins = Get-MsolRole -RoleName "Company Administrator"
+Get-MsolRoleMember -RoleObjectId $Global_Admins.ObjectId | ForEach-Object {
+    Set-MsolUser -UserPrincipalName $_.EmailAddress -StrongAuthenticationRequirements $MFA
+    }
